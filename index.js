440319c49cd8729da178f717eeba4c50dde78fb3
Erik Huang <erikhuangdrums@gmail.com>
2021-03-07 15:01:58 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = n => {
+    return Math.abs(n - 42);
+}
+
+const distanceFromHqInFeet = n => {
+    return distanceFromHqInBlocks(n) * 264;
+}
+
+const distanceTravelledInFeet = (x, y) => {
+    return Math.abs(y - x) * 264;
+}
+
+const calculatesFarePrice = (s, d) => {
+    let total = distanceTravelledInFeet(s, d);
+    if (total > 2500) {
+        return 'cannot travel that far';
+    }
+    if (total < 400) return 0;
+    if (total > 2000 && total <= 2500) {
+        return 25;
+    }
+    return ((total - 400) * .02);
+}
